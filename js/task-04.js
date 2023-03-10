db7f66d7af162e034d4828740d9964fc7a785473
KristinaYak <kristina.yakubovska@gmail.com>
2023-03-10 16:40:38 +0200

    add

--- js/task-04.js
+++ js/task-04.js
@@ -4,6 +4,14 @@
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
 
+// Лічильник складається зі спану і кнопок, які по кліку повинні збільшувати і зменшувати його значення на одиницю.
+
+// <div id="counter">
+//   <button type="button" data-action="decrement">-1</button>
+//   <span id="value">0</span>
+//   <button type="button" data-action="increment">+1</button>
+// </div>
+
 
 const AddDecBtn = document.querySelector('[data-action="decrement"]');
 console.log(AddDecBtn);
